967ba1f8a87677a204c3f29ab94d94cf1a62ff30
AbhishekRevoor1 <work.abhishek964@gmail.com>
2025-07-15 20:42:43 +0200

    RTL is handling all the instructions on Bare Metal-C v.1.3

--- rwu-rv64i-master/c_complile/C_Files/main.c
+++ rwu-rv64i-master/c_complile/C_Files/main.c
@@ -4,19 +4,21 @@
 #define GPIO_BASE   0x00010000UL
 
 #define DMEM_RESULT_OFFSET  16
-#define GPIO_OUTPUT_OFFSET  4
+#define GPIO_OUTPUT_OFFSET  0
+
 int main(void)
 {
     volatile uint64_t *dmem_result = (volatile uint64_t *)(DMEM_BASE + DMEM_RESULT_OFFSET);
     volatile uint8_t  *gpio_output = (volatile uint8_t *)(GPIO_BASE + GPIO_OUTPUT_OFFSET);
 
-    uint64_t sum = 0;
-
-    for (uint64_t i = 1; i <= 10; ++i) {
-        sum += i;
+    uint64_t sum;
+    
+    for (uint64_t i=0; i<1000; i++){
+    *dmem_result = i * 5;
     }
-
-    *dmem_result = sum;       
+    
+    sum = *dmem_result;
+    
     *gpio_output = (uint8_t)sum; 
     
 
